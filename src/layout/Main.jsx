c312416426df782a63e7dbd4edba08707ce6b230
Phanthom-Mekat <jahidulhossainmekat@gmail.com>
2024-11-14 00:26:03 +0600

    shadcn ui practice

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../component/header/Header";
 
+
 const Main = () => {
     return (
         <div>
             <Header/>
             <Outlet/>
+
         </div>
     );
 };
